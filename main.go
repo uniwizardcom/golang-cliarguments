a7362ccf7ed428037d372a663e3ba5d3fa377b12
wojtek-niewiadomski <wojciech.niewiadomski@miinto.com>
2022-02-11 14:01:14 +0100

    Example for pointer in main test

--- main.go
+++ main.go
@@ -6,16 +6,33 @@ import (
 	"strconv"
 )
 
+var cli *cliarguments.LineService
+
 func main() {
-	cli := cliarguments.LineService{
+	prepareArgs()
+
+	idv := cli.GetArgValue("insert-data", 0, "abc")
+	rlv := cli.GetArgValue("records-limit", 1, "777")
+	rov := cli.GetArgValue("records-offset", 1, "888")
+
+	rl, _ := strconv.ParseUint(rlv, 10, 64)
+	ro, _ := strconv.ParseUint(rov, 10, 64)
+	fmt.Printf("idv: [%s]\nrecords-limit=%d\nrecords-offset=%d\n\n", idv, rl, ro)
+}
+
+func prepareArgs() {
+	cli = &cliarguments.LineService{
 		Description: "Proof of concept for RediSearch functionality",
 		Using:       "Programu uzywa się standardowo ;)",
 	}
 	cli.SetItemSupported(cliarguments.LineServiceItem{
-		Name:      "insert-data",
-		Desc:      "Akcja dodająca-resetująca zestaw danych (łącznie z indexami) w Redis na podstawie danych z PostgreSQL",
-		IsRequire: true,
-		// DefaultValue: "insert",
+		Name: "insert-data",
+		Desc: "Akcja dodająca-resetująca zestaw danych (łącznie z indexami) w Redis na podstawie danych z PostgreSQL",
+	})
+	cli.SetItemSupported(cliarguments.LineServiceItem{
+		Name:  "make-test",
+		Level: 0,
+		Desc:  "Akcja odpalająca pakiet testów przeszukiwania",
 	})
 	cli.SetItemSupported(cliarguments.LineServiceItem{
 		Name:      "records-limit",
@@ -30,12 +47,4 @@ func main() {
 		IsRequire: true,
 	})
 	cli.ServiceCmdNew()
-
-	idv := cli.GetArgValue("insert-data", 0, "abc")
-	rlv := cli.GetArgValue("records-limit", 1, "777")
-	rov := cli.GetArgValue("records-offset", 1, "888")
-
-	rl, _ := strconv.ParseUint(rlv, 10, 64)
-	ro, _ := strconv.ParseUint(rov, 10, 64)
-	fmt.Printf("idv: [%s]\nrecords-limit=%d\nrecords-offset=%d\n\n", idv, rl, ro)
 }
